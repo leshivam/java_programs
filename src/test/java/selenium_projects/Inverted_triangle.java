2aa586e117f7a49df5d9fb48a0b14c1cc9f52e5a
Shivam Volvoicar <110778499+Shivameme17@users.noreply.github.com>
2025-06-03 23:41:40 +0530

    patterns

---
+++ src/test/java/selenium_projects/Inverted_triangle.java
@@ -0,0 +1,16 @@
+package selenium_projects;
+
+public class Inverted_triangle {
+    public static void main(String[] args) {
+        System.out.println("\n4. Inverted triangle:");
+        int rows;
+        for (int i = rows; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+    }
+}
---
+++ src/test/java/selenium_projects/Number_triangle.java
@@ -0,0 +1,12 @@
+package selenium_projects;
+
+public class Number_triangle {
+    public static void main(String[] args) {
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
