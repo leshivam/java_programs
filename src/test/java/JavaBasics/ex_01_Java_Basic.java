89775d06d783dce3216b3ef6e295961f47494c7f
Shivam Volvoicar <110778499+Shivameme17@users.noreply.github.com>
2025-06-07 20:22:59 +0530

    data types

---
+++ src/test/java/JavaBasics/ex_01_Java_Basic.java
@@ -0,0 +1,8 @@
+package JavaBasics;
+
+public class ex_01_Java_Basic {
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ src/test/java/JavaBasics/task11mar.java
@@ -0,0 +1,21 @@
+package JavaBasics;
+
+public class task11mar {
+    public static void main(String[] args) {
+        int a = 9;
+        System.out.printf("You variable is %d",a);
+        System.out.printf("You variable is %d",a*1);
+        System.out.printf("You variable is %d",a*2);
+        System.out.printf("You variable is %d",a*3);
+        System.out.printf("You variable is %d",a*4);
+        System.out.printf("You variable is %d",a*5);
+        System.out.printf("You variable is %d",a*6);
+        System.out.printf("You variable is %d",a*7);
+        System.out.printf("You variable is %d",a*8);
+        System.out.printf("You variable is %d",a*9);
+        System.out.printf("You variable is %d",a*10);
+
+
+
+    }
+}
