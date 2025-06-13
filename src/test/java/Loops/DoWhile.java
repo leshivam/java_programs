6fb787a2711288b8f2aebbc881166b2074819eee
Shivam Volvoicar <110778499+Shivameme17@users.noreply.github.com>
2025-06-13 23:42:19 +0530

    loops

---
+++ src/test/java/Loops/DoWhile.java
@@ -0,0 +1,13 @@
+package Loops;
+
+public class DoWhile {
+    public static void main(String[] args) {
+        int i = 0;
+        do {
+            System.out.println("Value of i: " + i);
+            i++;
+        } while (i < 5);
+    }
+
+
+}
---
+++ src/test/java/Loops/ForEach.java
@@ -0,0 +1,12 @@
+package Loops;
+
+public class ForEach {
+    public static void main(String[] args) {
+        String[] names = {"Alice", "Bob", "Charlie"};
+
+        // Using for-each loop to iterate over the array
+        for (String name : names) {
+            System.out.println(name);
+        }
+    }
+}
