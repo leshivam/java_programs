a82ac3cdbad5764ec009a0831b83399832585a2c
Shivam Volvoicar <110778499+Shivameme17@users.noreply.github.com>
2025-06-19 22:56:35 +0530

    ar st

---
+++ src/test/java/JavaBasics/Inheritance.java
@@ -0,0 +1,12 @@
+package JavaBasics;
+
+public class Inheritance {
+    public static void main(String[] args) {
+        // Create an instance of the subclass
+        Dog dog = new Dog();
+
+        // Call methods from the subclass and superclass
+        dog.bark(); // Output: Woof!
+        dog.eat();  // Output: Eating...
+    }
+}
